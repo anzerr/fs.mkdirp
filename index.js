7d43685228a0ea5a0953d654a8c8475a404bb4a0
ryan anzer <ryan.anzer@epitech.eu>
2019-01-02 17:07:42 +0100

    lol should work now?

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const mkdirP = (dir, option) => {
 	let p = path.resolve(dir);
 	return fs.mkdir(p, {...option, recursive: true}).catch((error) => {
 		if (error.code === 'ENOENT') {
-			return mkdirP(path.dirname(p), option);
+			return mkdirP(path.dirname(p), option).then(() => {
+				return fs.mkdir(p, {...option, recursive: true});
+			});
 		}
 		return fs.stat(p).then((stat) => {
 			if (!stat.isDirectory()) {
